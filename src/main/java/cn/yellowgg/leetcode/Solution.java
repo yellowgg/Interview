49fd4c6b5fe8a0c7a553f45a43889c6ff2e6a205
yellowgg <yellowgggg@gmail.com>
2019-06-17 16:37:58 +0800

    编程 leetcode

--- src/main/java/cn/yellowgg/leetcode/Solution.java
+++ src/main/java/cn/yellowgg/leetcode/Solution.java
@@ -199,6 +199,18 @@ public class Solution {
         }
     }
 
+    /**
+     * https://leetcode.com/problems/fibonacci-number/
+     */
+    public int fib(int N) {
+        int[] fib = new int[31];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < fib.length; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[N];
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
