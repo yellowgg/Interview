748250e2ab1c4da4ab2d08e9ec716af6be7bd300
yellowgg <yellowgggg@gmail.com>
2019-06-26 21:50:47 +0800

    编程
    leetcode

--- src/main/java/cn/yellowgg/leetcode/Solution.java
+++ src/main/java/cn/yellowgg/leetcode/Solution.java
@@ -357,6 +357,22 @@ public class Solution {
         }
     }
 
+    /**
+     * https://leetcode.com/problems/range-sum-of-bst/submissions/
+     */
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root==null) {
+            return 0;
+        }
+        if(root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        }
+        if(root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        }
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+    }
+    
     public static void main(String[] args) {
         Solution solution = new Solution();
         String s = solution.reverseWords("Let's take LeetCode contest");
